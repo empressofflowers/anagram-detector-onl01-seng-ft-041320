013f10d602717a7bf1e9539c8c2b2734f521cd8b
Devone Austin <devone.austin@gmail.com>
2020-04-24 18:55:15 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,7 +7,7 @@ class Anagram
   end
 
   def match(array)
-      array.select do |check|
-          check.split("").sort == @word.split("").sort
+      array.select do |word|
+          word.split("").sort == word.split("").sort
   end
 end
